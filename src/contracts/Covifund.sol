d3250054394d2dc6c37248c9182746f4a38a14f9
abelzach <abelsimonz@gmail.com>
2021-05-29 00:44:52 +0530

    Initial Smart contract

--- src/contracts/Covifund.sol
+++ src/contracts/Covifund.sol
@@ -31,4 +31,19 @@ contract Covifund {
         _user.userAddress = msg.sender;
         users[uCount] = _user;
     }
+    function initializeOrg(string memory _title, string memory _category, string memory _description) public {
+        require(bytes(_title).length > 0);
+        require(bytes(_description).length > 0);
+        oCount++;
+        orgs[oCount] = Org(oCount, 0, _title, _category, _description, msg.sender);
+    }
+    
+    function fundOrg(uint _id) public payable {
+        require(_id > 0 && _id <= oCount);
+        Org memory _org = orgs[_id];
+        address payable _org_address = _org.org_address;
+        address(_org_address).transfer(msg.value);
+        _org.funds = _org.funds + msg.value;
+        orgs[_id] = _org;
+    }
 }
