622b4483cd841a2ab03f978a1b0ba534b92fff5a
abelzach <abelsimonz@gmail.com>
2021-05-29 00:58:36 +0530

    Integrated portis

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import Portis from '@portis/web3';
+import Web3 from 'web3';
+
+const portis = new Portis('ad8c37c0-13cf-4d29-939e-221cf2ca9ead', 'maticMumbai');
+const web3 = new Web3(portis.provider);
 
 class App extends Component {
   render() {
