b691a17b47b18b5c3dc717e2a8bc8163f8df4ead
abelzach <abelsimonz@gmail.com>
2021-05-29 01:10:58 +0530

    Integrated matic

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 import Portis from '@portis/web3';
 import Web3 from 'web3';
+import Covifund from '../abis/Covifund.json'
 
 const portis = new Portis('ad8c37c0-13cf-4d29-939e-221cf2ca9ead', 'maticMumbai');
 const web3 = new Web3(portis.provider);
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadBlockchainData()
+  }
+  async loadBlockchainData() {
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = Covifund.networks[networkId]
+    if(networkData) {
+      const dvf = web3.eth.Contract(Covifund.abi, networkData.address)
+      this.setState({ dvf })
+      const uCount = await dvf.methods.uCount().call()
+      const oCount = await dvf.methods.oCount().call()
+      this.setState({ dvf })
+      for (var i = 1; i <= uCount; i++) {
+        const user = await dvf.methods.users(i).call()
+        console.log(user);
+        this.setState({
+          users: [...this.state.users, user]
+        })
+      }
+      for (var ib = 1; ib <= oCount; ib++) {
+        const org = await dvf.methods.orgs(ib).call()
+        this.setState({
+          orgs: [...this.state.orgs, org]
+        })
+      }
+      this.setState({ loading: false})
+    } else {
+      window.alert('The forum contract could not be deployed to network')
+    }
+  }
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <h1>Oxygen</h1>
+          <h1>Covifund</h1>
         </nav>
       </div>
     );
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 require('babel-register');
 require('babel-polyfill');
 
@@ -5,9 +9,16 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      port: 8545,
+      network_id: "*" 
     },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, "https://rpc-mumbai.maticvigil.com"),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
