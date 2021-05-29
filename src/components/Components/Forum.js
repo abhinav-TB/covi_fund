1f3c533c91884faa26f4b808ff7f52b3f9ce0b2d
abhinav-TB <abhinavtb@gmail.com>
2021-05-29 19:45:44 +0530

    ui changes

--- src/components/Components/Forum.js
+++ src/components/Components/Forum.js
@@ -5,94 +5,105 @@ import Grid from '@material-ui/core/Grid';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import RangeSlider from 'react-bootstrap-range-slider';
+import background from '../img/fourm_back.jpg'
 import './Forum.css'
 import '../App.css';
 
 
 class Forum extends Component {
-    constructor() {
-      super();
-      this.state = {
-        search: '',
-        amount: 10
-      };
-    }
-  
-    updateSearch(event) {
-      this.setState({search: event.target.value.substr(0,20)});
-    }
-  
-    render() {
-      let filteredOrgs = this.props.orgs.filter(
-        (org) => {
-          return org.title.indexOf(this.state.search) !== -1;
-        }
-      );
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      amount: 10
+    };
+  }
+
+  updateSearch(event) {
+    this.setState({ search: event.target.value.substr(0, 20) });
+  }
+
+  render() {
+    let filteredOrgs = this.props.orgs.filter(
+      (org) => {
+        return org.title.indexOf(this.state.search) !== -1;
+      }
+    );
 
     return (
-      <div className="wrapper">
-       <Navbar/>
+      <div className="wrapper back-img">
+        <Navbar />
         <div className="container-fluid mt-5 " >
-        
-        <div className="row">
+
+          <div className="row">
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
               <div className="content mr-auto ml-auto">
                 <p>&nbsp;</p>
-                <h1>Search <br/><br/>NGO <br/><br/>Individual Posts </h1>
+                <div className="eleven">
+                  <h1>Fund For A Noble Cause</h1>
+                </div>
+{/* 
+                <h1>Search <br /><br />NGO <br /><br />Individual Posts </h1> */}
                 <br></br>
-                <input type="text" class="form-control" value={this.state.search} onChange={this.updateSearch.bind(this)} />
+                  <div className="search-box">
+                    <input type="text" className="search-input " placeholder="Search.."  value={this.state.search} onChange={this.updateSearch.bind(this)} />
+                    <button className="search-button">
+                      <i className="fa fa-search" />
+                    </button>
+                  </div>
+
                 <p>&nbsp;</p>
-                { filteredOrgs.map((org, key) => {
-                  return(
+                {filteredOrgs.map((org, key) => {
+                  return (
                     <div key={key} >
-                      <div className="card-header">
-                      <p class="badge badge-primary">{org.category}</p>
-                      <br/>
-                      <h2>{org.title}</h2>
-                      <small>Address: {org.org_address}</small>
+                      <div className="card-header" >
+                        <p class="badge badge-primary">{org.category}</p>
+                        <br />
+                        <h2>{org.title}</h2>
+                        <small>Address: {org.org_address}</small>
                       </div>
-                      <p>Funds Received: ${window.web3.utils.fromWei((org.funds*430).toString(), 'Ether')} </p>
+                      <p>Funds Received: ${window.web3.utils.fromWei((org.funds * 430).toString(), 'Ether')} </p>
                       <ul id="postList" className="list-group list-group-flush">
                         <li className="list-group-item">
                           <p>{org.description}</p>
-                          <br/>
+                          <br />
                         </li>
                         <Grid item xs={12}>
-                        <form  style={{width: "100%", marginTop: 30}} 
-                        onSubmit={(event) => {
-                            event.preventDefault()
-                            let fundrec = window.web3.utils.toWei((this.state.amount/430).toString(), 'Ether')
-                            console.log(org.id.toString())
-                            this.props.fundOrg(org.id.toString(), fundrec)
-                        }}>
-                        <FormGroup>
-                            <FormLabel>How much would you like to contribute?</FormLabel>
-                            <FormLabel style={{marginTop: 30, display: 'flex', flexDirection: 'column', alignItems: 'center', fontWeight: '500', fontSize: '20px'}}>${this.state.amount}</FormLabel>
-                            <RangeSlider
+                          <form style={{ width: "100%", marginTop: 30 }}
+                            onSubmit={(event) => {
+                              event.preventDefault()
+                              let fundrec = window.web3.utils.toWei((this.state.amount / 430).toString(), 'Ether')
+                              console.log(org.id.toString())
+                              this.props.fundOrg(org.id.toString(), fundrec)
+                            }}>
+                            <FormGroup>
+                              <FormLabel>How much would you like to contribute?</FormLabel>
+                              <FormLabel style={{ marginTop: 30, display: 'flex', flexDirection: 'column', alignItems: 'center', fontWeight: '500', fontSize: '20px' }}>${this.state.amount}</FormLabel>
+                              <RangeSlider
                                 value={this.state.amount}
-                                onChange={e => this.setState({amount: e.target.value})}
+                                onChange={e => this.setState({ amount: e.target.value })}
                                 min={10}
                                 max={100}
-                                />
-                        </FormGroup>
-                        <br/>
-                        <button type="submit" className="btn btn-outline-info">Yes
+                              />
+                            </FormGroup>
+                            <br />
+                            <button type="submit" className="btn btn-outline-info">Yes
                         </button>
-                        </form>
+                          </form>
                         </Grid>
-                        </ul>
-                      <br/><br/>
+                      </ul>
+                      <br /><br />
                     </div>
                   )
                 })}
               </div>
             </main>
           </div>
-          <Footer/>
+          <Footer />
         </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  export default Forum;
+}
+
+export default Forum;
