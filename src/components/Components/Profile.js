f15e70d8707de159f9918410e7f4ea27a6a53986
abelzach <abelsimonz@gmail.com>
2021-05-29 16:05:55 +0530

    Register page

---
+++ src/components/Components/Profile.js
@@ -0,0 +1,104 @@
+import React, { useEffect ,Component} from 'react';
+import Navbar from './Home/navbar';
+import Footer from './Home/footer';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Box from '@material-ui/core/Box';
+import {colors} from "@material-ui/core";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CameraIcon from '@material-ui/icons/Favorite';
+import './profile.css'
+import { Doughnut } from "react-chartjs-2";
+const ringData = {
+    datasets: [
+      {
+        data: [50, 20, 30, 0, 0],
+        backgroundColor: [
+          colors.indigo[500],
+          colors.red[600],
+          colors.orange[600],
+          colors.green[600],
+          colors.yellow[600],
+        ],
+        borderWidth: 8,
+        borderColor: colors.common.white,
+        hoverBorderColor: colors.common.white,
+      },
+    ],
+    labels: ["EuroHealthNet","FEANTSA","Caritas Europa","Center for Social Inclusion","Oxfam India "],
+  };
+class Profile extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+            isuser: false
+        };
+    }
+
+    
+
+    render() {
+            return(
+                <>
+                <Navbar/>
+                <div className="container-fluid mt-5">
+                
+                <CssBaseline />
+              
+                <main>
+                    <div style={{paddingTop: 80, paddingBottom: 60}}>
+                    <Container maxWidth="sm">
+                        <Typography component="h3" variant="h4" align="center" color="textPrimary" gutterBottom>
+                        Hi, 
+                        <center>
+                        {this.props.account}
+                            </center>
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                        Welcome to your Profile! 
+                        <br></br>Here, you can view and manage your current donation records.
+                        </Typography>
+                    </Container>
+                     <center>
+                    <div container spacing={4} className="org-card">
+                        <Grid xs={12} sm={6} md={4}>
+                            <Card style={{height: '100%', display: 'flex' , }}>
+                            <CardContent style={{flexGrow: 1}} >
+                                <Typography gutterBottom variant="h5" component="h2" align='center'>3
+                                </Typography>
+                                
+                                <Typography>
+                                Organizations/Individuals Supported
+                                </Typography>
+                               
+                            </CardContent>
+                            </Card>
+                        </Grid>
+                        
+                    </div>
+                    </center>
+                    </div>
+                </main>
+                <Box height={300} position="relative">
+                        <Doughnut data={ringData}  />
+                    </Box>
+                <Footer/>
+                </div>
+                </> 
+            );
+        }
+       
+    
+}
+
+export default Profile;
---
+++ src/components/Components/Register.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+class Register extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="back" >
+                <Navbar/>
+                <div className="center">
+                <center>
+                    <h1 style={{color: 'white'}}><strong>Covifund</strong></h1>
+                    </center>
+                
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline />
+                        <div style={{marginTop: 80, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                            <Avatar style={{margin:10}}>
+                            <img style={{height: 40, width: 40}} src="https://thumbs.dreamstime.com/z/diverse-hands-holding-word-diversity-40595458.jpg"/>
+                            </Avatar>
+                            
+                            <Typography component="h1" variant="h5">
+                                <h1 style={{color: 'white'}}> Welcome New User</h1>
+                           
+                            </Typography>
+                            <Button
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                style = {{marginTop: 30, marginBottom: 20}}
+                                onClick = {this.props.initializeUser}
+                            >
+                                Add me as New User
+                            </Button>
+                        </div>
+
+                    </Container>
+                    </div>   
+                <Footer/>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Register;
